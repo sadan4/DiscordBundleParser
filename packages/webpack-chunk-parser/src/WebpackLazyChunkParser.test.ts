900c9d586ae6f14d6060aab6a6ffeff57b9bf77a
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-03 11:41:57 -0500

    test1

--- packages/webpack-chunk-parser/src/WebpackLazyChunkParser.test.ts
+++ packages/webpack-chunk-parser/src/WebpackLazyChunkParser.test.ts
@@ -4,22 +4,32 @@ import { getFile } from "./__test__/testingUtil";
 import { WebpackLazyChunkParser } from "./WebpackLazyChunkParser";
 
 describe("LazyChunkParser", function () {
-    it("gets modules from a lazy chunk", function () {
-        const parser = new WebpackLazyChunkParser(getFile("lazyChunk.js"));
-        const modules = parser.getDefinedModules();
+    describe("old format", () => {
+        it("gets modules from a lazy chunk", function () {
+            const parser = new WebpackLazyChunkParser(getFile("lazyChunk.js"));
+            const modules = parser.getDefinedModules();
 
-        expect(modules).toMatchSnapshot();
+            expect(modules).toMatchSnapshot();
+        });
+        it("gets modules from an i18n chunk", function () {
+            const parser = new WebpackLazyChunkParser(getFile("lazyChunk-i18n.js"));
+            const modules = parser.getDefinedModules();
+
+            expect(modules).toMatchSnapshot();
+        });
+        it("gets chunk id from a lazy chunk", function () {
+            const parser = new WebpackLazyChunkParser(getFile("lazyChunk.js"));
+            const { chunkId } = parser;
+
+            expect(chunkId).toMatchSnapshot();
+        });
     });
-    it("gets modules from an i18n chunk", function () {
-        const parser = new WebpackLazyChunkParser(getFile("lazyChunk-i18n.js"));
-        const modules = parser.getDefinedModules();
+    describe("new format", () => {
+        it("gets modules from a lazy chunk", () => {
+            const parser = new WebpackLazyChunkParser(getFile("lazyChunk2.js"));
+            const modules = parser.getDefinedModules();
 
-        expect(modules).toMatchSnapshot();
-    });
-    it("gets chunk id from a lazy chunk", function () {
-        const parser = new WebpackLazyChunkParser(getFile("lazyChunk.js"));
-        const { chunkId } = parser;
-
-        expect(chunkId).toMatchSnapshot();
+            expect(modules).toMatchSnapshot();
+        });
     });
 });
